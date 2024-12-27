c928b78f370efead950b90f4eaa9ee52f2e2aaa8
meghnastudio <mobarokmukut.personal@gmail.com>
2024-12-27 17:12:30 +0600

    fixed

--- app/src/main/java/com/mobarok/progressbar/MainActivity.java
+++ app/src/main/java/com/mobarok/progressbar/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.mobarok.roundprogressbar.RoundProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
